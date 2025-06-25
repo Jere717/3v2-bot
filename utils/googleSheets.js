2123eec24d81f77d1bb5a90a3ebd90b70b7a6940
Jere717 <cardenasjeremias573@gmail.com>
2025-06-25 19:35:50 -0300

    Volviendo atras.

--- utils/googleSheets.js
+++ utils/googleSheets.js
@@ -4,20 +4,10 @@
 
 // const { google } = require('googleapis');
 // const sheets = google.sheets('v4');
-const fetch = require('node-fetch');
-
-// Cambia esta URL por la de tu App Script desplegado
-const SHEET_CONFIG_URL = 'https://script.google.com/macros/s/AKfycbytspTzI1qZvRT3uVWoFp9jEdNjuAa0sy4WIDNVXbE8nv4ps7WvE3QCK9Rha3NVhf-P/exec';
 
 async function getSheetConfig() {
-  const res = await fetch(SHEET_CONFIG_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ op: 'obtenersheet' })
-  });
-  const data = await res.json();
-  if (data.status !== '0') throw new Error('No se pudo obtener la configuración del Sheet: ' + data.message);
-  return data.configuracion;
+  // Implementa autenticación y lectura de la hoja "Configuracion"
+  return {};
 }
 
 module.exports = { getSheetConfig };
